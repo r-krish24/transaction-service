f7578d4f7742ea412fa848ee4cefd043648658fc
Ram Krishnan <ramkrishnane@maveric-systems.com>
2022-08-26 14:47:44 +0530

    Resolved review comments

--- src/main/java/com/maveric/transactionservice/model/Transaction.java
+++ src/main/java/com/maveric/transactionservice/model/Transaction.java
@@ -1,16 +1,14 @@
 package com.maveric.transactionservice.model;
 
 import com.maveric.transactionservice.constants.Type;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 @Document(collection = "Transaction")
